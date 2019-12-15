8c03ee78fa8596d9ae5c834dcb0062b66feebe1c
cincity2019 <cnkelly73@gmail.com>
2019-12-15 17:48:53 -0500

    finsh

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cardFromIndex = document.querySelector('cards');
+
+axios
+	.get('https://api.github.com/users/cincity2019')
+	.then((response) => {
+		//console.log(response);
+		let obj = response.data;
+		document.querySelector('.cards').appendChild(CardCreator(obj));
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,12 +35,7 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
+/*
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -44,7 +51,7 @@ const followersArray = [];
   </div>
 </div>
 
-*/
+
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -53,3 +60,51 @@ const followersArray = [];
   luishrd
   bigknell
 */
+function CardCreator(object) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	const img = document.createElement('img');
+	img.src = object['avatar_url'];
+	card.appendChild(img);
+
+	const cardInfo = document.createElement('div');
+	cardInfo.classList.add('card-info');
+	card.appendChild(cardInfo);
+
+	const h3 = document.createElement('h3');
+	h3.classList.add('name');
+	h3.textContent = object.name;
+	cardInfo.appendChild(h3);
+
+	const username = document.createElement('p');
+	username.classList.add('username');
+	username.textContent = object.login;
+	cardInfo.appendChild(username);
+
+	const location = document.createElement('p');
+	location.textContent = `Location: ${object.location}`;
+	cardInfo.appendChild(location);
+
+	const profile = document.createElement('p');
+	profile.textContent = 'Profile:';
+	cardInfo.appendChild(profile);
+
+	const url = document.createElement('a');
+	url.href = object.url;
+	profile.appendChild(url);
+
+	const followers = document.createElement('p');
+	followers.textContent = `Followers: ${object.followers}`;
+	cardInfo.appendChild(followers);
+
+	const following = document.createElement('p');
+	following.textContent = `Following: ${object.following}`;
+	cardInfo.appendChild(following);
+
+	const bio = document.createElement('p');
+	bio.textContent = `Bio: ${object.bio}` || 'This user does not have a bio yet.';
+	cardInfo.appendChild(bio);
+
+	return card;
+}
